aa97119a8779c06de2b5888b83ca358de2af15aa
Rafael <rafaelmreyes@hotmail.com>
2023-02-16 16:02:56 -0700

    first commit

---
+++ webpack/src/index.js
@@ -0,0 +1,55 @@
+const $link = document.getElementById('links')
+const $name = document.querySelector('h1')
+
+const data = 
+{
+    name: "Rafael Morey",
+    nickname: "rafa",
+    description: "...",
+    avatar: "...",
+    social: 
+    [
+      {
+        name: "twitter",
+        url: "https://twitter.com/",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    
+    links: 
+    [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Colombia",
+};
+
+const main = ()=>{
+    let name = document.createTextNode(data?.name)
+    let links = data?.links?.map((network)=>{
+        return `
+        <div class${link.color}-200 px-4 py-5 w-full flex justify-between">
+        <a class="text-sm font-bold text-${link.color}-600 text-center hover:text-${link.color}-800 cursor-pointer"
+          href="${link.url}" target="_blank">
+          ${link.name}
+        </a>
+        <span>${link.emoji}</span>
+      </div>
+      `
+    })
+}
